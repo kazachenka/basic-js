67cb2bc92e09e7c948cf76fd32c7640fda7f4118
kazachenka <57540722+kazachenka@users.noreply.github.com>
2020-11-01 22:49:00 +0300

    fix: fix solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample( sampleActivity ) {
-  let res = ( sampleActivity != +sampleActivity || +sampleActivity > 15 || +sampleActivity <= 0) ? false : +sampleActivity;
+  let res = ( sampleActivity != +sampleActivity || +sampleActivity >= 15 || +sampleActivity <= 0) ? false : +sampleActivity;
   let k = Math.log(MODERN_ACTIVITY / res) / ( 0.693 / HALF_LIFE_PERIOD);
-  result = res == false ? false : Math.round(k);
+  result = res == false ? res : Math.ceil(k);
   return result;
 };
