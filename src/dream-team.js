ea01236e45fad1b3327fad5cc4aade7f8755fc9d
kazachenka <57540722+kazachenka@users.noreply.github.com>
2020-11-01 23:08:34 +0300

    fix: fix solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,7 @@
 module.exports = function createDreamTeam( members ) {
   let team = '';
-  
   for(let i = 0; i < members.length;i++){
    ( members[i].split('')[0].charCodeAt() >= 65 && members[i].split('')[0].charCodeAt() <= 90) ? team += members[i][0].split('') : team;
   }
-  return res = team === '' ? false : team.split('').sort().join('');
+  return res = team == '' ? false : team.split('').sort().join('');
 };
