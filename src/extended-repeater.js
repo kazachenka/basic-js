4df1219905fc143e841fdda494c0f63a0ca7d511
kazachenka <57540722+kazachenka@users.noreply.github.com>
2020-11-01 22:56:05 +0300

    fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-module.exports = function repeater(/* str, options */) {
+module.exports = function repeater( str, options ) {
     let res = '';
     for (let i = 1; i <= options.repeatTimes;i++){
         res += str
