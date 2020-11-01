c5b658578dc2e2b5040c40168df308abd6ab1e2f
kazachenka <57540722+kazachenka@users.noreply.github.com>
2020-11-01 22:49:51 +0300

    add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
 module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let res = '';
+    for (let i = 1; i <= options.repeatTimes;i++){
+        res += str
+        for (let j = 1; j < options.additionRepeatTimes+1;j++){
+            res += options.addition;
+            if (j !== options.additionRepeatTimes){
+                res += options.additionSeparator;
+            }
+        }
+        if (i !== options.repeatTimes){
+         res += options.separator;
+        }
+        
+    }
+    return res;
 };
   
